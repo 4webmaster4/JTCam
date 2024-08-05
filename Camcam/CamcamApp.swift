0cdeff3badd80d79826927f58470b77f5707a7e0
Ilya <4webmaste4@gmail.com>
2024-08-05 12:51:53 +0200

    Initial Commit

---
+++ Camcam/CamcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamcamApp.swift
+//  Camcam
+//
+//  Created by Ilya Fomenko on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CamcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamcamTests/CamcamTests.swift
@@ -0,0 +1,17 @@
+//
+//  CamcamTests.swift
+//  CamcamTests
+//
+//  Created by Ilya Fomenko on 05/08/2024.
+//
+
+import Testing
+@testable import Camcam
+
+struct CamcamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
